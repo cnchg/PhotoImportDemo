221b149f9e476c9f696a9ff404973174fa2e2954
“Ori <chandleretienne@gmail.com>
2017-01-12 16:57:54 -0600

    Update

--- app/src/main/java/com/tricloudcommunications/ce/photoimportdemo/MainActivity.java
+++ app/src/main/java/com/tricloudcommunications/ce/photoimportdemo/MainActivity.java
@@ -33,6 +33,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1){
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                getPhoto();
+
+            }
+
+        }
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
